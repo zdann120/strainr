5b173c0ef7c98a3085935aac3402eb1d85c76715
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-01-31 15:15:21 -0800

    fix mail in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :strains, foreign_key: :created_by
   has_many :ratings
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # 
+  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_URL'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:                 587,
